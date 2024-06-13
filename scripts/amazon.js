64ae4e81e4bf04b8af72715352c88d9fc839bfca
Farshad Davari <DavariFarshad3@outlook.com>
2024-06-13 19:28:49 +0330

    Show added to cart when click add to cart.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach(product => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -89,6 +89,9 @@ document.querySelectorAll(".js-add-to-cart-button")
 
       document.querySelector(".js-cart-quantity")
         .innerHTML = cartQuantity;
+
+      document.querySelector(`.js-added-to-cart-${productId}`)
+        .classList.add("added-to-cart-visibility");
     });
 });
 
