52456c44d69f7d44ec22ffcf1b994479d1818f39
Farshad Davari <DavariFarshad3@outlook.com>
2024-06-19 20:29:31 +0330

    Import repeated code for calculating cartQuantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -63,12 +63,9 @@ document.querySelector(".js-products-grid").innerHTML = html;
 const addedMessageTimeouts = {};
 
 function updateCartQuantity(productId) {
-  let cartQuantity = 0;
   const addedToCartMessage = document.querySelector(`.js-added-to-cart-${productId}`);
 
-  cart.forEach(cartItem => {
-    cartQuantity += cartItem.quantity;
-  });
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector(".js-cart-quantity")
     .innerHTML = cartQuantity;
